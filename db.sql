21386a09c12072c04209eb2c979510e0aadd5756
huseyiniris <huseyiniris@gmail.com>
2025-03-02 01:18:55 +0300

    feat(listing): add image upload limit and database schema
    - Add image upload limit validation in listing routes
    - Create database schema for user listings and photos- Implement check for maximum 3 images per listing
    - Set up models and services for user and listing data

---
+++ db.sql
@@ -0,0 +1,46 @@
+create table showcase
+(
+    id       int auto_increment
+        primary key,
+    platinum tinyint null,
+    gold     tinyint null,
+    silver   tinyint null
+);
+
+create table userlisting
+(
+    id            int auto_increment
+        primary key,
+    userId        int           null,
+    showcaseIndex int           null,
+    title         varchar(1024) null,
+    description   text          null,
+    phone         varchar(255)  null
+);
+
+create table userlistingphoto
+(
+    id            int auto_increment
+        primary key,
+    showcaseIndex int           null,
+    userId        int           null,
+    path          varchar(1024) null
+);
+
+create table users
+(
+    id       int auto_increment
+        primary key,
+    username varchar(255)      null,
+    password varchar(255)      null,
+    isAdmin  tinyint default 0 null
+);
+
+create table usershowcase
+(
+    id            int auto_increment
+        primary key,
+    userId        int null,
+    showcaseIndex int null
+);
+
--- routes/listing.routes.js
+++ routes/listing.routes.js
@@ -2,6 +2,8 @@ const express = require("express");
 const ListingController = require("../controllers/listing.controller");
 const {verifyToken} = require("../middleware/auth.middleware");
 const upload = require("../utils/imageUpload");
+const UserServices = require("../services/user.services");
+const UserListingPhotoModel = require("../models/userlistingphoto.model");
 
 const router = express.Router();
 
@@ -13,7 +15,17 @@ router.get("/:showcaseIndex/images", verifyToken, ListingController.images);
 router.post(
     "/:showcaseIndex/image",
     verifyToken,
-    (req, res, next) => {
+    async (req, res, next) => {
+        const user = await UserServices.getUserByUsername(req.user.username);
+        const images = await UserListingPhotoModel.count({
+            where: {
+                userId: user.id,
+                showcaseIndex: req.params.showcaseIndex,
+            }
+        });
+        if (images >= 3) {
+            return res.status(400).json({message: 'En fazla 3 resim yükleyebilirsiniz!'});
+        }
         upload.single('image')(req, res, (err) => {
             if (err) {
                 return res.status(400).json({message: err.message});
