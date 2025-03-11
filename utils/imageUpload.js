1cd9a325cd10f78c89a836bc9376dbee5c6665c9
huseyiniris <huseyiniris@gmail.com>
2025-03-11 21:02:09 +0300

    feat(imageUpload): create images directory if it doesn't exist
    - Add fs module to handle file system operations
    - Implement check and create images directory if it doesn't exist
    - Ensure that the images directory is always available for file uploads

--- utils/imageUpload.js
+++ utils/imageUpload.js
@@ -1,9 +1,14 @@
 const multer = require('multer');
 const path = require('path');
+const fs = require("node:fs");
 
 
 const storage = multer.diskStorage({
+
     destination: function (req, file, cb) {
+        if (!fs.existsSync('images/')) {
+            fs.mkdirSync('images/');
+        }
         cb(null, 'images/');
     },
     filename: function (req, file, cb) {
